1f4f79b978a0bfb307c2da2140363d6647e82d64
Konstantin S. <havendv@gmail.com>
2026-09-13 15:09:35 +0400

    feat: Updated OpenAPI spec (#49)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/AsyncAI/Generated/AsyncAI.ITextToSpeechClient.Authorizations.ApiKeyInHeader.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace AsyncAI
+{
+    public partial interface ITextToSpeechClient
+    {
+        /// <summary>
+        /// Authorize using ApiKey authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingApiKeyInHeader(
+            string apiKey);
+    }
+}
